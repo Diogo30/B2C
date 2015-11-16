6c9b5a8ae448ee8d6e1f46d4ca29f700417ff962
Diogo <diogofpmadureira@gmail.com>
2015-11-16 15:28:35 +0000

    Project B2C added

---
+++ www/js/main.js
@@ -0,0 +1,21 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+var app = angular.module('starter', ['ionic'])
+
+app.controller('MainCtrl', function($scope, $ionicSideMenuDelegate){
+  $scope.toggleLeft = function(){
+    $ionicSideMenuDelegate.toggleLeft()
+  }
+})
+
+app.controller('MenuList', function($scope){
+  $scope.items = [
+    {name: 'Home'},
+    {name: 'Pictures'},
+    {name: 'Videos'},
+    {name: 'Articles'}
+  ]
+})
